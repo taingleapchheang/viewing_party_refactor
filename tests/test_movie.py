f060477f743d3d952d77933ec63e71b2dd25337b
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-04-01 10:49:07 -0700

    update movie and person

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,24 @@
 import pytest
+from viewing_party.movie import Movie
+
+    # def __init__(self, title, genre, rating, host):
+    #     self.title = title
+    #     self.genre = genre
+    #     self.rating = rating
+    #     self.host = host
+    
+    # def update_rating(self, new_rating):
+    #     self.rating = new_rating
+
+
 
 def test_1():
     # Arrange
+    movie = Movie("The JavaScript and the React","Action", 2.2, "Netflix")
 
     # Act
+    movie.update_rating(5)
 
     # Assert
-    pass
+    assert movie.rating == 5
+
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,38 @@
 import pytest
+from viewing_party.person import Person
+from viewing_party.movie import Movie
+
+    # def __init__(self, name, watched,friends,subscriptions):
+    #     self.name = name
+    #     self.watched = watched
+    #     self.friends = friends
+    #     self.subscriptions = subscriptions
+    # def add_watched(self,movie):
+    #     self.watched.append(movie)
+    # def get_num_watched(self):
+    #     return len(self.watched)
+
 
 def test_1():
     # Arrange
+    movie1 = Movie("The JavaScript and the React","Action", 2.2, "Netflix")
+    movie2 = Movie("Zero Dark Python","Intrigue", 3, "Disney")
+    person = Person("Tom",[movie1],[],"Hulu")
 
     # Act
+    person.add_watched(movie2)
 
     # Assert
-    pass
+    assert movie2 in person.watched
+    assert len(person.watched) == 2
+
+def test_2():
+    # Arrange
+    movie1 = Movie("The JavaScript and the React","Action", 2.2, "Netflix")
+    person = Person("Tom",[movie1],[],"Hulu")
+
+    # Act
+    count = person.get_num_watched()
+
+    # Assert
+    assert count == 1
